188a81633a436407a51abf77c00cb57b9049359b
TaiNguyen <vannguyents@gmail.com>
2019-07-10 17:28:46 -0500

    Lab12

---
+++ lab12/W3D2-SEC/src/main/java/edu/mum/cs544/SpringSecurityInitializer.java
@@ -0,0 +1,7 @@
+//package edu.mum.cs544;
+//
+//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//
+//public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+//    //no code needed
+//}
